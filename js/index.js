255ec6b3f646065f3030bb6865344a2cc1a92cf1
Lucifer <piushbasu@gmail.com>
2022-06-10 22:08:46 +0530

    Managed product section

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 const prodNavItm = document.querySelectorAll(".product__nav__item")
 const prodNav = document.querySelector(".product__nav")
 
+// TAB LOOKING LIKE NAVIGATION STYLING FOR ACTIVE TABS
 prodNavItm.forEach((element) => {
     element.addEventListener("click", () => {
         prodNav.querySelector(".active").classList.remove("active")
