80d26af22c4c4840efff48fc7bc9b9f05b2d7eb7
Lucifer <piushbasu@gmail.com>
2022-06-25 22:20:14 +0530

    Made up with some javascript

--- js/index.js
+++ js/index.js
@@ -1,5 +1,8 @@
 const prodNavItm = document.querySelectorAll(".product__nav__item")
 const prodNav = document.querySelector(".product__nav")
+const mainSection = document.querySelector("#product__section")
+const scrollUp = document.querySelector("#scrollUp")
+const allProducts = document.querySelectorAll("div[id^='product']")
 
 // TAB LOOKING LIKE NAVIGATION STYLING FOR ACTIVE TABS
 prodNavItm.forEach((element) => {
@@ -7,4 +10,31 @@ prodNavItm.forEach((element) => {
         prodNav.querySelector(".active").classList.remove("active")
         element.classList.add("active")
     })
+})
+
+const observer = new IntersectionObserver(
+    (entries, _) => {
+        entries.forEach((entry) => {
+            if(!entry.isIntersecting) {
+                scrollUp.classList.remove("visible")
+                scrollUp.classList.add("hidden");
+            } else {
+                scrollUp.classList.remove("hidden")
+                scrollUp.classList.add("visible");
+            }
+        })
+    }
+)
+observer.observe(mainSection)
+
+scrollUp.addEventListener("click", () => {
+    window.scrollTo(0, 0);
+
+    // OR
+
+    // window.scroll({
+    //     top: 0,
+    //     left: 0,
+    //     behavior: "smooth",
+    // })
 })
